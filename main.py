e85b77ef35c302cb7e05dc5e310084d75375fe76
umidjonsangilov <usangilov.96@gmail.com>
2026-06-26 22:57:30 +0500

    port

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ app.include_router(diagnostic_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7535, reload=True)
