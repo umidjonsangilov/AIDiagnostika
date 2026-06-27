2b16a1c3b60834b38cf8ba2e6416fd1be02fd8a0
umidjonsangilov <usangilov.96@gmail.com>
2026-06-27 09:49:37 +0500

    db mysql -> postgresql

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase,sessionmaker
 from data.config import DB_USER, DB_PASS, DB_HOST, DB_NAME
 
-database_url = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+database_url = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 engine = create_engine(database_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
